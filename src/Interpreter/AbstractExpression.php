b65bfd97dc40c6fe50db9143850fccbb0f5ceb30
Yagur Dmitriy <yagurdmitriy@gmail.com>
2017-06-25 18:56:41 +0300

    Added logic for `AbstractExpression` and `InterpreterContext`

--- src/Interpreter/AbstractExpression.php
+++ src/Interpreter/AbstractExpression.php
@@ -1,13 +1,34 @@
 <?php
 
-namespace TestTemplatizer;
+namespace TestTemplatizer\Interpreter;
 
 /**
  * Class AbstractExpression
  * @package TestTemplatizer
  */
-class AbstractExpression
+abstract class AbstractExpression
 {
+    /** @var int  */
+    private  static  $keycount = 0;
+    /** @var int */
+    private $key;
 
+    /**
+     * @param InterpreterContext $context
+     * @return mixed
+     */
+    abstract function interpret(InterpreterContext $context);
 
+    /**
+     * @return int
+     */
+    public function getKey()
+    {
+        if (!isset($this->key)) {
+            self::$keycount++;
+            $this->key=self::$keycount;
+        }
+
+        return $this->key;
+    }
 }
