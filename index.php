ad88a1cb1e0ea4aecda00f0191143230a6d5c9e3
Yagur Dmitriy <yagurdmitriy@gmail.com>
2017-06-25 23:23:19 +0300

    Addded Iterable conditions.

--- index.php
+++ index.php
@@ -10,18 +10,24 @@ $statement = new \TestTemplatizer\Interpreter\Statements\IfConditionalStatementE
     new \TestTemplatizer\Interpreter\Operators\ToBoolExpression($input),
     new \TestTemplatizer\Interpreter\Statements\IfConditionalStatementExpression(
         new \TestTemplatizer\Interpreter\Operators\ToBoolExpression($input2),
-        new \TestTemplatizer\Interpreter\Expressions\LiteralExpression('test3')
+        new \TestTemplatizer\Interpreter\IterableExpressions\ListExpression([
+            new \TestTemplatizer\Interpreter\Expressions\LiteralExpression('ttafgsfghtttttttttt'),
+            new \TestTemplatizer\Interpreter\Expressions\LiteralExpression('546745674567'),
+            new \TestTemplatizer\Interpreter\Expressions\LiteralExpression('546745674dfghdfghdfghdfghe567'),
+            new \TestTemplatizer\Interpreter\Expressions\LiteralExpression('qqqqqqqqqqqqqqqqqqqqqqqqqqq'),
+            new \TestTemplatizer\Interpreter\Expressions\LiteralExpression('vvvvvvvvvvvvvvvvvvvvvvvvvvvv'),
+        ])
     ),
     new \TestTemplatizer\Interpreter\Expressions\LiteralExpression('test2')
 );
 
-$input->setValue('sdf');
-$input2->setValue('1');
+$input->setValue(true);
+$input2->setValue(true);
 $statement->interpret($context);
 var_dump($context->lookup($statement));
 
 
-$input->setValue('ertwetr');
+$input->setValue(false);
 $input2->setValue('dsfasd');
 $statement->interpret($context);
 var_dump($context->lookup($statement));
